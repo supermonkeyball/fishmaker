35724a0f118c88da0c5b2906944711abd1ac6fc7
Glitch (glitch-hello-website) <none>
2022-04-21 23:53:20 +0000

    🙌🐘 Checkpoint ./script.js:22332890/1588 ./index.html:22332890/798

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function species() {
   let slider2 =  document.getElementById("slider2");
   let value = slider2.value;
   
-  if (value === 1) {
+  while (value > 1) {
     document.getElementById("fish").src = "https://cdn.glitch.global/d99d0544-47ec-4043-bd39-1e149e6c6393/bass.glb?v=1650581382210";
   }
 }
